8d4b0f3962d49c65ec1bfd4502ebc21d9dca135c
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 00:35:31 +0100

    Update AddCaptchaService method logic to register HTTP client service

--- src/CaptchaService.cs
+++ src/CaptchaService.cs
@@ -5,7 +5,7 @@ using Microsoft.JSInterop;
 namespace ChatAIze.Captcha;
 
 [method: ActivatorUtilitiesConstructor]
-public sealed class CaptchaService(IOptions<CaptchaOptions> options, IJSRuntime jsRuntime) : IAsyncDisposable
+public sealed class CaptchaService(HttpClient httpClient, IJSRuntime jsRuntime, IOptions<CaptchaOptions> options) : IAsyncDisposable
 {
     private readonly Lazy<Task<IJSObjectReference>> moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/ChatAIze.Captcha/captcha.js").AsTask());
 
@@ -23,6 +23,7 @@ public static class CaptchaServiceExtensions
 {
     public static IServiceCollection AddCaptchaService(this IServiceCollection services, Action<CaptchaOptions> configureOptions)
     {
+        services.AddHttpClient<CaptchaService>();
         services.AddScoped<CaptchaService>();
         services.Configure(configureOptions);
 
