18b88f2228974d6d3c3b3d144a6735d67451c4b2
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-10 23:56:09 +0100

    Update captcha service to receive captcha options

--- src/CaptchaService.cs
+++ src/CaptchaService.cs
@@ -1,9 +1,11 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.JSInterop;
 
 namespace ChatAIze.Captcha;
 
-public sealed class CaptchaService(IJSRuntime jsRuntime) : IAsyncDisposable
+[method: ActivatorUtilitiesConstructor]
+public sealed class CaptchaService(IOptions<CaptchaOptions> options, IJSRuntime jsRuntime) : IAsyncDisposable
 {
     private readonly Lazy<Task<IJSObjectReference>> moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/ChatAIze.Captcha/captcha.js").AsTask());
 
@@ -19,9 +21,11 @@ public sealed class CaptchaService(IJSRuntime jsRuntime) : IAsyncDisposable
 
 public static class CaptchaServiceExtensions
 {
-    public static IServiceCollection AddCaptchaService(this IServiceCollection services)
+    public static IServiceCollection AddCaptchaService(this IServiceCollection services, Action<CaptchaOptions> configureOptions)
     {
         services.AddScoped<CaptchaService>();
+        services.Configure(configureOptions);
+
         return services;
     }
 }
