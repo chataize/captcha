befe8461acd6ec4ab5a3681f0433d8ccd55025f1
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 02:44:51 +0100

    Add theme parameter to initCaptcha method

--- src/wwwroot/captcha.js
+++ src/wwwroot/captcha.js
@@ -1,9 +1,10 @@
-export function initCaptcha(id, reference, siteKey) {
+export function initCaptcha(id, reference, siteKey, theme) {
     let captcha = document.getElementById(id);
     if (!captcha) return;
 
     window.hcaptcha.render(captcha, {
         sitekey: siteKey,
+        theme: theme,
         callback: function (token) {
             reference.invokeMethodAsync('OnCaptchaCompleted', token);
         }
