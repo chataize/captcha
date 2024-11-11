23522d60227ea6a817711f5126e3792aa26d69c5
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 01:42:52 +0100

    Add InitializeAsync and VerifyTokenAsync methods to CaptchaService class

--- src/CaptchaService.cs
+++ src/CaptchaService.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.JSInterop;
@@ -8,6 +9,33 @@ namespace ChatAIze.Captcha;
 public sealed class CaptchaService(HttpClient httpClient, IJSRuntime jsRuntime, IOptions<CaptchaOptions> options) : IAsyncDisposable
 {
     private readonly Lazy<Task<IJSObjectReference>> moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/ChatAIze.Captcha/captcha.js").AsTask());
+
+    public async Task InitializeAsync(int id, DotNetObjectReference<Captcha> reference)
+    {
+        var module = await moduleTask.Value;
+        await module.InvokeVoidAsync("initCaptcha", id, reference);
+    }
+
+    public async Task<bool> VerifyTokenAsync(string token)
+    {
+        var requestContent = new FormUrlEncodedContent(
+        [
+            new KeyValuePair<string, string>("secret", options.Value.Secret),
+            new KeyValuePair<string, string>("sitekey", options.Value.SiteKey),
+            new KeyValuePair<string, string>("response", token),
+        ]);
+
+        var response = await httpClient.PostAsync("https://hcaptcha.com/siteverify", requestContent);
+        if (!response.IsSuccessStatusCode)
+        {
+            return false;
+        }
+
+        var responseContent = await response.Content.ReadAsStreamAsync();
+        var responseDocument = await JsonDocument.ParseAsync(responseContent);
+
+        return responseDocument.RootElement.GetProperty("success").GetBoolean();
+    }
 
     public async ValueTask DisposeAsync()
     {
