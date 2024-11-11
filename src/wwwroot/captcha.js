f46a624ab9252adb049a0e1ae8578047fb050068
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 02:50:14 +0100

    Add size parameter to initCaptcha function

--- src/wwwroot/captcha.js
+++ src/wwwroot/captcha.js
@@ -1,10 +1,11 @@
-export function initCaptcha(id, reference, siteKey, theme) {
+export function initCaptcha(id, reference, siteKey, theme, size) {
     let captcha = document.getElementById(id);
     if (!captcha) return;
 
     window.hcaptcha.render(captcha, {
         sitekey: siteKey,
         theme: theme,
+        size: size,
         callback: function (token) {
             reference.invokeMethodAsync('OnCaptchaCompleted', token);
         }
