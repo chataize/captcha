db02e830a82c3219077111d8e41a2a1b53556f74
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 02:28:28 +0100

    Add VerifyIpAddresses property to CaptchaOptions record

--- src/CaptchaOptions.cs
+++ src/CaptchaOptions.cs
@@ -7,13 +7,16 @@ public sealed record CaptchaOptions
     public CaptchaOptions() { }
 
     [SetsRequiredMembers]
-    public CaptchaOptions(string siteKey, string secret)
+    public CaptchaOptions(string siteKey, string secret, bool verifyIpAddresses = false)
     {
         SiteKey = siteKey;
         Secret = secret;
+        VerifyIpAddresses = verifyIpAddresses;
     }
 
     public required string SiteKey { get; set; }
 
     public required string Secret { get; set; }
+
+    public bool VerifyIpAddresses { get; set; }
 }
