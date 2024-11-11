3572a41af088b0dcfb052a67372cdb3c9a8a9454
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 02:30:40 +0100

    Ensure IsConnectionProxied is true before checking for X-Forwarded-For

--- src/CaptchaMiddleware.cs
+++ src/CaptchaMiddleware.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace ChatAIze.Captcha;
 
 public sealed class CaptchaMiddleware(RequestDelegate next)
 {
-    public async Task InvokeAsync(HttpContext context, CaptchaService captchaService)
+    public async Task InvokeAsync(HttpContext context, CaptchaService captchaService, IOptions<CaptchaOptions> options)
     {
-        if (context.Request.Headers.TryGetValue("X-Forwarded-For", out var forwardedFor))
+        if (options.Value.IsConnectionProxied && context.Request.Headers.TryGetValue("X-Forwarded-For", out var forwardedFor))
         {
             captchaService.IpAddress = forwardedFor;
         }
