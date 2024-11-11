42cd3f6d18920db62cd43cdcc935eb6e9f0934be
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 02:01:40 +0100

    Fix using string captcha instead of actual captcha element

--- src/wwwroot/captcha.js
+++ src/wwwroot/captcha.js
@@ -2,7 +2,7 @@ export function initCaptcha(id, reference, siteKey) {
     let captcha = document.getElementById(id);
     if (!captcha) return;
 
-    window.hcaptcha.render('captcha', {
+    window.hcaptcha.render(captcha, {
         sitekey: siteKey,
         callback: function (token) {
             reference.invokeMethodAsync('OnCaptchaCompleted', token);
