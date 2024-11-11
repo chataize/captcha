f0ac0de659d410ecc21324ad3913e16959194a03
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 03:11:44 +0100

    Add captcha events

--- src/wwwroot/captcha.js
+++ src/wwwroot/captcha.js
@@ -8,6 +8,21 @@ export function initCaptcha(id, reference, siteKey, theme, size) {
         size: size,
         callback: function (token) {
             reference.invokeMethodAsync('OnCaptchaCompleted', token);
+        },
+        'open-callback': function () {
+            reference.invokeMethodAsync('OnCaptchaOpened');
+        },
+        'close-callback': function () {
+            reference.invokeMethodAsync('OnCaptchaClosed');
+        },
+        'expired-callback': function () {
+            reference.invokeMethodAsync('OnCaptchaExpired');
+        },
+        'chalexpired-callback': function () {
+            reference.invokeMethodAsync('OnCaptchaChallengeExpired');
+        },
+        'error-callback': function (code) {
+            reference.invokeMethodAsync('OnCaptchaError', code);
         }
     });
 }
